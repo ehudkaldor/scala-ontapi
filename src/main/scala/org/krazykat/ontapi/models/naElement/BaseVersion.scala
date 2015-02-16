0e0dfe3d1ff85ad7d8b88a0dc924a735fd5018c2
ehud <ehud.kaldor@gmail.com>
2015-02-16 15:34:06 -0800

    changed to abstract class (to prevent instances)

--- src/main/scala/org/krazykat/ontapi/models/naElement/BaseVersion.scala
+++ src/main/scala/org/krazykat/ontapi/models/naElement/BaseVersion.scala
@@ -3,6 +3,6 @@ package org.krazykat.ontapi.models.naElement
 import org.krazykat.ontapi.models.naElement.basic.NodeVersionDetailInfo
 import org.krazykat.ontapi.models.naElement.basic.SystemVersionTuple
 
-class BaseVersion(version: String, isClustered: Boolean) extends NaElement {
+abstract class BaseVersion(version: String, isClustered: Boolean) extends NaElement {
   val api = "system-get-version"
 }
