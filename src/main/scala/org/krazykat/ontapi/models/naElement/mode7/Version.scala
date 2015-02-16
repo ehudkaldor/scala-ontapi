91c82d82a6e7fda55edbdb440918287be02a1c42
ehud <ehud.kaldor@gmail.com>
2015-02-16 00:39:48 -0800

    cosmetics

--- src/main/scala/org/krazykat/ontapi/models/naElement/mode7/Version.scala
+++ src/main/scala/org/krazykat/ontapi/models/naElement/mode7/Version.scala
@@ -1,4 +1,5 @@
 package org.krazykat.ontapi.models.naElement.mode7
+
 import org.krazykat.ontapi.models.naElement.BaseVersion
 
 case class Version(version: String, isClustered: Boolean) extends BaseVersion(version, isClustered)
